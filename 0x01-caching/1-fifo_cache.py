8e0b654cad1b063e100de8ad81ff0765c36ac346
mostsfa538 <mostafa51moktar@gmail.com>
2024-08-20 22:39:03 +0300

    1-fifo_cache.py

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""  FIFO caching """
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ FIFOCache class """
+
+    def put(self, key, item):
+        """ assign to the dictionary self.cache_data """
+        if key is None or item is None:
+            return
+
+        self.cache_data[key] = item
+        if len(self.cache_data.keys()) > super().MAX_ITEMS:
+            remove = list(self.cache_data.keys())[0]
+            print(f'DISCARD: {remove}')
+            del self.cache_data[key]
+
+    def get(self, key):
+        """ return the value in self.cache_data linked to key """
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data.get(key)
