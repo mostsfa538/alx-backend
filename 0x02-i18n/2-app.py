0b3b8274f4a2e2256bfd81da70bdd53564989cab
mostsfa538 <mostafa51moktar@gmail.com>
2024-08-27 22:51:37 +0300

    2-app

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -17,6 +17,7 @@ class Config:
 app.config.from_object(Config)
 
 
+@babel.localeselector
 def get_locale():
     """ determine the best match with our supported languages """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
