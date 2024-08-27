ffe8c47e099fb6393996a0899e7bd0b871f7b109
mostsfa538 <mostafa51moktar@gmail.com>
2024-08-27 23:14:48 +0300

    4-app

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -20,13 +20,15 @@ app.config.from_object(Config)
 @babel.localeselector
 def get_locale():
     """ determine the best match with our supported languages """
+    if 'locale' in request.args.keys():
+        return request.args['locale']
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def hello():
     """ index """
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
