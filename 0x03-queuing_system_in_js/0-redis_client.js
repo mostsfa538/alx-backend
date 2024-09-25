dd70d1d607250f955b56ffbb1f6c7b6eb6d6d705
mostsfa538 <mostafa51moktar@gmail.com>
2024-09-25 09:20:36 +0300

    Node Redis client and basic operations

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,11 @@
+import redis from 'redis';
+
+let client = redis.createClient({ url: 'redis://localhost:6379' });
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err.message}`);
+});
